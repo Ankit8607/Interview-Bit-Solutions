3bb0d78893c345ef5faa0cd7ea7f228930ead4e3
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2022-04-02 10:41:50 +0530

    Update 3 Repeat Number.cpp

--- Array/3 Repeat Number.cpp
+++ Array/3 Repeat Number.cpp
@@ -13,49 +13,64 @@ Output : 1
 
 */
 
-int Solution::repeatedNumber(const vector<int> &A) {
-    int n=A.size();
-    int e1=INT_MAX,e2=INT_MAX;
-    int count1=0,count2=0;
-    for(int i=0;i<n;i++){
-    	if(count1>0 && A[i]==e1){
-    		count1+=1;
-    	}
-    	else if(count2>0 && A[i]==e2){
-    		count2+=1;
-    	}
-    	else if(count1==0){
-    		e1=A[i];
-    		count1=1;
-    	}
-    	else if(count2==0){
-    		e2=A[i];
-    		count2=1;
-    	}
-    	else{
-    		count1--;
-    		count2--;
-    	}
+int Solution::repeatedNumber(const vector<int> &arr) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    int n=arr.size();
+    int count1 = 0, count2 = 0;
+    int first=INT_MAX    , second=INT_MAX    ;
+ 
+    for (int i = 0; i < n; i++) {
+ 
+        // if this element is previously seen,
+        // increment count1.
+        if (first == arr[i])
+            count1++;
+ 
+        // if this element is previously seen,
+        // increment count2.
+        else if (second == arr[i])
+            count2++;
+     
+        else if (count1 == 0) {
+            count1++;
+            first = arr[i];
+        }
+ 
+        else if (count2 == 0) {
+            count2++;
+            second = arr[i];
+        }
+ 
+        // if current element is different from
+        // both the previously seen variables,
+        // decrement both the counts.
+        else {
+            count1--;
+            count2--;
+        }
     }
-    if(count1==0 && count2==0)
-    	return -1;
-    int freq=0;
-   if(count1){
-	for(int i=0;i<n;i++){
-    	if(A[i]==e1)
-    		freq++;
+ 
+    count1 = 0;
+    count2 = 0;
+ 
+    // Again traverse the array and find the
+    // actual counts.
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == first)
+            count1++;
+ 
+        else if (arr[i] == second)
+            count2++;
     }
-    if(freq>n/3)
-    	return e1;
-   }
-    freq=0;
-  if(count2){
-    for(int i=0;i<n;i++){
-    	if(A[i]==e2)
-    		freq++;
-    }
-    if(freq>n/3)
-    	return e2;
-  }
+ 
+    if (count1 > n / 3)
+        return first;
+ 
+    if (count2 > n / 3)
+        return second;
+ 
     return -1;
 }
