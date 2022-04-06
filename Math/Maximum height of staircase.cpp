d35bdf9a612d5a176425d1dbfb3490bb4eba5f9b
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2022-04-06 13:51:29 +0530

    Create Maximum height of staircase.cpp

---
+++ Math/Maximum height of staircase.cpp
@@ -0,0 +1,59 @@
+/*
+
+Given an integer A representing the number of square blocks. The height of each square block is 1. The task is to create a staircase of max height using these blocks.
+
+The first stair would require only one block, the second stair would require two blocks and so on.
+
+Find and return the maximum height of the staircase.
+
+
+
+Problem Constraints
+0 <= A <= 109
+
+
+Input Format
+The only argument given is integer A.
+
+
+
+Output Format
+Return the maximum height of the staircase using these blocks.
+
+
+
+Example Input
+Input 1:
+
+ A = 10
+Input 2:
+
+ 20
+
+
+Example Output
+Output 1:
+
+ 4
+Output 2:
+
+ 5
+ 
+ 
+*/
+
+
+
+int Solution::solve(int A) {
+    int ans=0;
+    int lo=0,hi=2*A;
+    while(lo<=hi){
+        int mid=(lo+hi)/2;
+        if((1LL*mid*mid+mid)<=2*A){
+            lo=mid+1;
+            ans=mid;
+        }
+        else hi=mid-1;
+    }
+    return ans;
+}
