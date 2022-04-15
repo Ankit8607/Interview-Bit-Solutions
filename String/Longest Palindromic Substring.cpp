b0a25906d3f1cd648f2683a088150ac3ce49d501
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2022-04-15 09:17:24 +0530

    Create Longest Palindromic Substring.cpp

---
+++ String/Longest Palindromic Substring.cpp
@@ -0,0 +1,84 @@
+/*
+
+Given a string A of size N, find and return the longest palindromic substring in A.
+
+Substring of string A is A[i...j] where 0 <= i <= j < len(A)
+
+Palindrome string:
+
+A string which reads the same backwards. More formally, A is palindrome if reverse(A) = A.
+
+Incase of conflict, return the substring which occurs first ( with the least starting index).
+
+
+
+Input Format
+First and only argument is a string A.
+
+
+
+Output Format
+Return a string denoting the longest palindromic substring of string A.
+
+
+
+Example Input
+A = "aaaabaaa"
+
+
+Example Output
+"aaabaaa"
+
+
+Example Explanation
+We can see that longest palindromic substring is of length 7 and the string is "aaabaaa".
+
+*/
+// Manacher Algorithm
+
+string Solution::longestPalindrome(string A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    int start = 0;
+    int length = A.size();
+    int maxLen = 1;
+    
+    int low = 0, high;
+    
+    for(int i = 0; i < length; i++){
+        low = i-1;
+        high = i;
+        while((low >= 0) && (high < length) && (A[low] == A[high])){
+            if((high - low + 1) > maxLen){
+                maxLen = high-low+1;
+                start = low;
+            }        
+            low--;
+            high++;
+        }
+        
+        low = i-1;
+        high = i+1;
+        while((low >= 0) && (high < length) && A[low] == A[high]){
+            if((high - low + 1) > maxLen){
+                maxLen = high-low+1;
+                start = low;
+            }
+            low--;
+            high++;
+        }
+    }
+    
+    string sol = "";
+    
+    high = start+maxLen;
+    while(start != high){
+        sol = sol + A[start];
+        start++;
+    }
+    
+    return sol;
+}
