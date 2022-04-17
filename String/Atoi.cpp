482bafb9ca3a0975fe9f7812b2fc75c338d7398d
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2022-04-17 11:01:41 +0530

    Update Atoi.cpp

--- String/Atoi.cpp
+++ String/Atoi.cpp
@@ -35,6 +35,25 @@ Warning : DO NOT USE LIBRARY FUNCTION FOR ATOI.
 If you do, we will disqualify your submission retroactively and give you penalty points.
 */
 
+
+// Smart way
+
+int Solution::atoi(const string A) {
+stringstream ss;
+ss<<A;
+long long i;
+ss>>i;
+if(i<INT_MIN)
+i=INT_MIN;
+if(i>INT_MAX)
+i=INT_MAX;
+return i;
+}
+
+
+// Other Approach
+
+
 int Solution::atoi(const string A) {
     int ans=0,neg=1;
     for(int i=0;i<A.size();i++){
