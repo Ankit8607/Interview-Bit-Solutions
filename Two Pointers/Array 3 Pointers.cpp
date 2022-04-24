e25ca2cb5c96eba52755d939bd1c80512924b7e0
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2022-04-24 09:45:01 +0530

    Update Array 3 Pointers.cpp

--- Two Pointers/Array 3 Pointers.cpp
+++ Two Pointers/Array 3 Pointers.cpp
@@ -66,8 +66,8 @@ int Solution::minimize(const vector<int> &a, const vector<int> &b, const vector<
   int diff = INT_MAX;
         int minimum = INT_MAX;
         int maximum = INT_MIN;
-        int i, j, k;
-        for(i = 0, j = 0, k = 0; i < a.size() && j < b.size() && k < c.size();) {
+        int i=0, j=0, k=0;
+        while(i < a.size() && j < b.size() && k < c.size()) {
             minimum = min(a[i], min(b[j], c[k]));
             maximum = max(a[i], max(b[j], c[k]));
             diff = min(diff, maximum - minimum);
