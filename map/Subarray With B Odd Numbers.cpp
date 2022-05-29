fbb2536dee90911aa2711684d1dde141bb5f05ea
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2022-05-29 11:14:49 +0530

    Create Subarray With B Odd Numbers.cpp

---
+++ map/Subarray With B Odd Numbers.cpp
@@ -0,0 +1,76 @@
+// https://www.interviewbit.com/problems/subarray-with-b-odd-numbers/
+
+/*
+
+Problem Description
+
+Given an array of integers A and an integer B.
+
+Find the total number of subarrays having exactly B odd numbers.
+
+
+
+Problem Constraints
+1 <= length of the array <= 105
+
+1 <= A[i] <= 109
+
+0 <= B <= A
+
+
+
+Input Format
+The first argument given is the integer array A.
+
+The second argument given is integer B.
+
+
+
+Output Format
+Return the total number of subarrays having exactly B odd numbers.
+
+
+
+Example Input
+Input 1:
+
+ A = [4, 3, 2, 3, 4]
+ B = 2
+Input 2:
+
+ A = [5, 6, 7, 8, 9]
+ B = 3
+
+
+Example Output
+Output 1:
+
+ 4
+Output 2:
+
+ 1
+
+
+Example Explanation
+Explanation 1:
+
+ The subarrays having exactly B odd numbers are:
+ [4, 3, 2, 3], [4, 3, 2, 3, 4], [3, 2, 3], [3, 2, 3, 4]
+Explanation 2:
+
+ The subarrays having exactly B odd numbers is [5, 6, 7, 8, 9]
+ 
+ */
+
+
+int Solution::solve(vector<int> &A, int B) {
+    map<int,int>mp;
+    mp[0]=1;
+    int ans=0,odd=0,last=0;
+    for(int i=0;i<A.size();i++){
+        odd+=A[i]%2;
+        ans+=mp[odd-B];
+        mp[odd]++;
+    }
+    return ans;
+}
